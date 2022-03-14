013bf7523bb98a4161b28ecfb37d1ef51a366f28
sparsell <hello@sparsell.com>
2022-03-14 16:32:10 -0400

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,46 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+    class App extends React.Component {
+        constructor(props) {
+            super(props);
+
+            this.state = { lat: null, long: null };
+
+            window.navigator.geolocation.getCurrentPosition(
+                position => {
+                    this.setState({ lat: position.coords.latitude })
+                    this.setState({ long: position.coords.longitude })
+                },
+                err => {
+                    this.setState({errorMessage: err.message})
+                    console.log(err.message)
+                }
+                );
+        }
+
+        render() {
+            return (
+            <button>Push this!</button>
+            )
+            // if (this.state.errorMessage && !this.state.lat) {
+            //     return <div>Error: {this.state.errorMessage}</div>
+            // } 
+
+            // if (!this.state.errorMessage && this.state.lat) {
+            //     return <div>
+            //                 <p>Lat: {this.state.lat}</p>
+            //                 <p>Long: {this.state.long}</p>
+            //             </div>
+            // }
+
+            // return <div>Loading!</div>
+
+        }
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+<App />, 
+document.querySelector('#root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
